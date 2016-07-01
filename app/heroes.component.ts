c2f71aa1943dc178c9c2c2e09839c2cbc73388e6
Thomas Surgent <surgentt@gmail.com>
2016-07-01 16:17:47 -0400

    Gitignore is now funtioning

---
+++ app/heroes.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+// Importing the Hero Service
+// Don't ever heroService = new HeroService(); This could work, but is a bad idea.
+import { HeroService } from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `],
+  // Our template needs to know about my-hero-detail
+  directives: [HeroDetailComponent],
+  // Our template needs knowledge of the HeroService
+  providers: [HeroService]
+})
+
+// Export the App Component to be used in index.html
+// Grab the OnInit decorator and implement with ngOnInit.
+export class HereoComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  // Constructor: function Book(){} | var myBook = new Book();
+  // The constructor is for simple initializations like wiring constructor parameters to properties
+  // keep complex logic out of the constructor, especially anything that might call a server as a data access method is sure to do.
+  constructor(private heroService: HeroService) { }
+  // Call my ngOnInit. 
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+}
