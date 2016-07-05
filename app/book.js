e2b1039be535c0ab7171b21d8049f43234ee608d
Thomas Surgent <surgentt@gmail.com>
2016-07-05 10:36:51 -0400

    Attempting to rename files

--- app/book.js
+++
@@ -1,9 +0,0 @@
-function Book(name, year) {
-  this.name = name;
-  this.year = year;
-  this.verify = function () { return this.name === this.year; };
-}
-
-var myBook = new Book("1984", 1984);
-myBook.name
-myBook.verify
